818d52cedb064e5b562e74646626c2efcc8fa22b
Kihyun Lim <122237814+paraguayrussianblue@users.noreply.github.com>
2023-01-09 17:58:32 +0900

    업로드
    https://www.acmicpc.net/problem/17298

---
+++ 오큰수.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+
+int main(){
+    std::stack<int>srt;
+    int N;
+    std::cin>>N;
+    int A[N];
+    int NGE[N];
+    for(int i=0;i!=N;i++){
+        std::cin>>A[i];
+    }
+    for(int i=N-1;i>=0;i--){
+        while(!srt.empty()){
+            if(A[i]>=srt.top()){
+            srt.pop();
+            }
+            else{
+                NGE[i]=srt.top();
+                srt.push(A[i]);
+                goto end;
+            }
+        }
+            NGE[i]=-1;
+            srt.push(A[i]);
+    end: ;
+    }
+    for(int i=0;i!=N;i++){
+        std::cout<<NGE[i]<<' ';
+    }
+}
